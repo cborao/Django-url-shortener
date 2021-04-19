1b322a238dbe14cad6c7e321b9b64dd18e4e099f
cesarborao <alumno@ubuntu2004.localdomain>
2021-04-19 19:16:46 +0200

    Version final v1.0

--- project/acorta/views.py
+++ project/acorta/views.py
@@ -4,31 +4,32 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Content
 
+
 @csrf_exempt
 def index(request):
 
     if request.method == "POST":
 
+        # We check if the POST request have a valid query string
         if 'url' in request.POST and 'short' in request.POST:
             url = request.POST['url']
             short = request.POST['short']
         else:
-            return HttpResponse("POST not implemented without a querystring", status=501)
+            return HttpResponse("POST not implemented without a query string", status=501)
 
-        # We check if the url was typed including 'http://', 'https://' or not
+        # We check if the url was introduced including 'http://', 'https://' or not
         if not url.startswith('http://') and not url.startswith('https://'):
             # if the url dont't include it, we add it to the start of the url
             url = 'https://' + url
 
         try:
-            # upload content
+            # if the shortened url have been stored in a previous POST, we
+            # remove it and add the new content.
             content = Content.objects.get(key=short)
             content.delete()
         except Content.DoesNotExist:
             pass
 
-        print("URL: " + url)
-        print("short: " + short)
         content = Content(key=short, url=url)
         content.save()
 
@@ -39,6 +40,7 @@ def index(request):
 
 @csrf_exempt
 def get_content(request, shortened):
+
     try:
         content = Content.objects.get(key=shortened)
         context = {'content': content}
